98a06469ab1f443ee694291bbbcbf05ca37d126d
Oluwole Fadeyi <16049411+tfadeyi@users.noreply.github.com>
2023-05-20 19:27:53 +0100

    Second commit
    Signed-off-by: Oluwole Fadeyi <16049411+tfadeyi@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -3,24 +3,22 @@ ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 # include files with the `// +build mock` annotation
 TEST_TAGS:=-tags mock -coverprofile cover.out
 
-.PHONY: build generate test build-all-platforms clean install-ci-tools install-local-tools licenses
+.PHONY: generate test build-all-platforms clean install-ci-tools install-local-tools licenses
 
 install-ci-tools:
 	go install github.com/matryer/moq@v0.2.7
 	go install github.com/google/go-licenses@c781b427440f8ea100841eefdd308e660d26d121
 
 install-local-tools: install-ci-tools
+	go install github.com/atombender/go-jsonschema/cmd/gojsonschema@latest
 	go install github.com/goreleaser/goreleaser@v1.17.0
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
-
-build:
-	cd $(ROOT_DIR) && $(GO_BUILD) -o builds/$(BIN_NAME) .
 
 # used to generate struct mocks
 generate:
 	cd $(ROOT_DIR) && go generate ./...
 
-test: build
+test:
 	cd $(ROOT_DIR) &&  go test $(GOFLAGS) $(TEST_TAGS) ./...
 
 coverage: test
--- errors.go
+++ errors.go
@@ -1,51 +1,131 @@
+//go:generate rm -f ./pkg/api/api.go
+//go:generate gojsonschema -p api ./schema/schema.json -o ./pkg/api/api.go
+
 package goaloe
 
 import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/tfadeyi/go-aloe/internal/client"
 	"github.com/tfadeyi/go-aloe/internal/client/local"
 )
 
 type (
-	Aloe struct {
-		source string
+	errorHandler struct {
 		client client.Client
 		logger *log.Logger
 	}
 
+	// Options for the aloe error handler. Use it to configure the aloe error handler
 	Options struct {
-		Source string
+		// SourceFilename is the location of the file containing the aloe specification for the target service
+		SourceFilename string
+		// Source is the in-memory aloe specification for the target service
+		Source []byte
+		// Logger is the logger used by the error handler to report errors that might occur during execution.
+		// Set as nil, if no logging is wanted.
 		Logger *log.Logger
 	}
 )
 
-func newInstance(opts Options) *Aloe {
-	cl := local.NewLazy(opts.Source)
+const (
+	DefaultAloeFilename = "default.aloe"
+)
 
-	return &Aloe{
-		source: opts.Source,
+// newInstance generates a new instance of the aloe error handler
+func newInstance(opts Options) (*errorHandler, error) {
+	cl, err := local.New(opts.SourceFilename, opts.Source)
+	if err != nil {
+		return nil, err
+	}
+
+	return &errorHandler{
+		logger: opts.Logger,
+		client: cl,
+	}, nil
+}
+
+func lazyInstance(opts Options) *errorHandler {
+	cl := local.NewLazy(opts.SourceFilename, opts.Source)
+
+	return &errorHandler{
 		logger: opts.Logger,
 		client: cl,
 	}
 }
 
-func Default() *Aloe {
-	instance := newInstance(Options{
-		Source: "default.aloe.yaml",
-		Logger: log.Default(),
-	})
+// DefaultOrDie returns an instance of the Aloe error handler, using the default configuration, or panics.
+// It expects the aloe specification to be in the current working directory, as default.aloe.(yaml|json|toml) .
+func DefaultOrDie() *errorHandler {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Errorf("go-aloe was unable to get the current working directory: [%w]", err))
+	}
+
+	specs := []string{
+		fmt.Sprintf("%s/%s.toml", dir, DefaultAloeFilename),
+		fmt.Sprintf("%s/%s.yaml", dir, DefaultAloeFilename),
+		fmt.Sprintf("%s/%s.json", dir, DefaultAloeFilename)}
+	var instance *errorHandler
+
+	for _, src := range specs {
+		instance, err = newInstance(Options{
+			SourceFilename: src,
+			Logger:         log.Default(),
+		})
+		if err == nil {
+			break
+		}
+	}
+
+	if err != nil {
+		panic(fmt.Errorf("go-aloe default handler failed to initialise: [%w]", err))
+	}
+
 	return instance
 }
 
-func WithOptions(opts Options) *Aloe {
-	instance := newInstance(opts)
+// Default returns an instance of the Aloe error handler, using the default configuration, or errors.
+// It expects the aloe specification to be in the current working directory, as default.aloe.(yaml|json|toml) .
+func Default() (*errorHandler, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("go-aloe was unable to get current working directory: [%w]", err)
+	}
+
+	specs := []string{
+		fmt.Sprintf("%s/%s.toml", dir, DefaultAloeFilename),
+		fmt.Sprintf("%s/%s.yaml", dir, DefaultAloeFilename),
+		fmt.Sprintf("%s/%s.json", dir, DefaultAloeFilename)}
+	var instance *errorHandler
+
+	for _, src := range specs {
+		instance, err = newInstance(Options{
+			SourceFilename: src,
+			Logger:         log.Default(),
+		})
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("go-aloe default handler failed to initialise: [%w]", err)
+	}
+
+	return instance, nil
+}
+
+// WithOptions returns an instance of the Aloe error handler, with the given configuration.
+// Use this to remove logging or specify a different location for the Aloe specification file.
+func WithOptions(opts Options) *errorHandler {
+	instance := lazyInstance(opts)
 	return instance
 }
 
-func (instance *Aloe) ErrorWithContext(ctx context.Context, err error, code string) error {
+// ErrorWithContext wraps the incoming error with error defined by the Aloe specification according to the input code.
+// if no error is found in the specification, the original error is returned.
+func (instance *errorHandler) ErrorWithContext(ctx context.Context, err error, code string) error {
 	if err == nil {
 		return err
 	}
@@ -59,7 +139,9 @@ func (instance *Aloe) ErrorWithContext(ctx context.Context, err error, code stri
 	return fmt.Errorf("%s: [%w]", newErrMessage, err)
 }
 
-func (instance *Aloe) Error(err error, code string) error {
+// Error wraps the incoming error with error defined by the Aloe specification according to the input code.
+// if no error is found in the specification, the original error is returned.
+func (instance *errorHandler) Error(err error, code string) error {
 	if err == nil {
 		return err
 	}
@@ -73,8 +155,8 @@ func (instance *Aloe) Error(err error, code string) error {
 	return fmt.Errorf("%s: [%w]", newErrMessage, err)
 }
 
-func (instance *Aloe) log(msg string, keyVal ...any) {
+func (instance *errorHandler) log(msg string, keyVal ...any) {
 	if instance.logger != nil {
-		instance.logger.Printf(msg, keyVal)
+		instance.logger.Printf(msg, keyVal...)
 	}
 }
